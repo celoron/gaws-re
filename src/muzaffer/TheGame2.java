fb61d1b0be5ea4b9b0a684e2953b464af1dd23ee
Muzaffer YILMAZ <muzaffer.yilmz@gmail.com>
2013-02-01 16:55:04 +0200

    final4

--- src/muzaffer/TheGame2.java
+++ src/muzaffer/TheGame2.java
@@ -9,7 +9,7 @@ public class TheGame2 extends StateBasedGame {
 	
 	public TheGame2() {
         super("Test Muzaffer");
-/*final*/
+/*final3*/
         
         addState(new Scene());
         enterState(0);
